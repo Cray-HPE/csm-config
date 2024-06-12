f88672a66db26c09f2f9fb3775d93204b056c490
ravikanth-nalla-hpe <140072234+ravikanth-nalla-hpe@users.noreply.github.com>
2024-06-12 23:04:16 +0530

    CASMPET-6797: Ansible play to provision LIO services on worker node(s) (#232)
    * CASMCMS-8924: Fixed typo in ansible/ims_computes.yml
    
    * Update CHANGELOG.md
    
    Release 1.17.10 for CSM 1.6
    
    * - CASMPET-6797(EPIC): Add Ansible plays to install/config/provision LIO services on iSCSI targets (worker NCNs) for SBPS
      - CASMPET-6886: Add Ansible play to install RPM packages (iSCSI LIO target + targetcli) required for SBPS
      - CASMPET-6887: Add Ansible play to configure LIO targets
      - CASMPET-6888: Add Ansible play to add/ update DNS "SRV" and "A" records for HSN and NMN using play script against PowerDNS API
      - CASMPET-6890: Add Ansible play to mount s3 bucket 'boot-images' using s3fs read-only policy for SBPS
      - CASMPET-6933: Add Ansible play to apply k8s label for SBPS on specified number of worker NCN's
    
    * - review comments incorporated
    
    * - review comments incorporated
    
    * - updated CHANGELOG
    
    * - review comments incorporated
    
    * - updated ../CHANGELOG.md
    
    * CASMPET-6980: Enable/ start SBPS marshal agent systemd service during… (#269)
    
    * CASMPET-6980: Enable/ start SBPS Marshal agent systemd service during worker node personalization
    
      - Enable/ start SBPS Marshal agent systemd service (post install) during
        worker node personalization.
    
    * - review comments incorporated
    
    * Apply suggestions from code review
    
    Linting
    
    * Apply suggestions from code review
    
    Update version date
    
    ---------
    
    Co-authored-by: Mitch Harding (the weird one) <mitchell.harding@hpe.com>

---
+++ ansible/roles/csm.sbps.dns_srv_records/files/sbps_get_host_hsn_nmn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2024 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+set -euo pipefail
+
+# Get Host Name, HSN IP and NMN IP of worker node
+host_name="$(awk '{print $1}' /etc/hostname)"
+hsn_ip="$(ip addr | grep "hsn0$" | awk '{print $2;}' | awk -F\/ '{print $1;}')"
+nmn_ip="$(ip addr | grep "nmn0$" | awk '{print $2;}' | awk -F\/ '{print $1;}')"
+
+# echo the details to stdout to be picked by next task in the playbook
+echo "$host_name:$hsn_ip:$nmn_ip"
