fed58351725c6c4c0c53a979527464bf32e494f2
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2025-07-09 18:00:09 -0400

    CASMPET-7616: Modify iSCSI playbook to be aware of new HSM group

--- ansible/roles/csm.sbps.dns_srv_records/files/sbps_get_host_hsn_nmn.sh
+++ ansible/roles/csm.sbps.dns_srv_records/files/sbps_get_host_hsn_nmn.sh
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2024 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2024-2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -25,9 +25,9 @@
 
 set -euo pipefail
 
-get-token ()
+function get-token ()
 {
-    export TOKEN=$(curl -s -S -d grant_type=client_credentials -d client_id=admin-client -d client_secret=`kubectl get secrets admin-client-auth -o jsonpath='{.data.client-secret}' | base64 -d` "https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token" | jq -r '.access_token')
+    TOKEN=$(curl -s -S -d grant_type=client_credentials -d client_id=admin-client -d client_secret=`kubectl get secrets -n default admin-client-auth -o jsonpath='{.data.client-secret}' | base64 -d` "https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token" | jq -r '.access_token')
 }
 
 function authcurl()
@@ -36,16 +36,17 @@ function authcurl()
     curl -H "Authorization: Bearer ${TOKEN}" "$@"
 }
 
-function xname2hostname()
+function hostname2xname()
 {
-    local XNAME="$1"
-    authcurl -sk -X GET https://api-gw-service-nmn.local/apis/sls/v1/hardware/${XNAME} | jq .ExtraProperties.Aliases[] -r | head -n 1
+    local HNAME="$1"
+    authcurl -sk -X GET https://api-gw-service-nmn.local/apis/bss/boot/v1/bootparameters | jq -r '.[] | select(."cloud-init"."meta-data"."local-hostname" == "'${HNAME}'") | .hosts[0]' | head -n 1
 }
 
-for h in $( authcurl -s "https://api-gw-service-nmn.local/apis/smd/hsm/v2/State/Components?role=Management&subrole=Worker" | jq .Components[].ID -r ); do
-    NMN_IP="$( host $h | grep "has address" | tail -n 1 | awk '{ print $NF }' )"
-    HSN_IP="$( host ${h}h0 | grep "has address" | tail -n 1 | awk '{ print $NF }' || true )"
-    RHOST="$( xname2hostname $h )"
+# Use the presence of the iscsi Kubernetes label as the basis of which hosts to include
+for h in $(kubectl get nodes -l iscsi=sbps -o custom-columns=:.metadata.name --no-headers); do
+    xname=$(hostname2xname "${h}")
+    NMN_IP="$( host $xname | grep "has address" | tail -n 1 | awk '{ print $NF }' )"
+    HSN_IP="$( host ${xname}h0 | grep "has address" | tail -n 1 | awk '{ print $NF }' || true )"
 
-    echo $RHOST:$HSN_IP:$NMN_IP
-done | sort
+    echo $h:$HSN_IP:$NMN_IP
+done | sort -u
